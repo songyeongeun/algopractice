a60a2b2815e9392bf0f210d5e06f88391e3c0f63
Yeongeun Song <ye3557@naver.com>
2023-09-05 12:32:46 +0900

    [ac]#Week1 14681

---
+++ Week1/[ac]14681.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int x, y;
+    cin >> x >> y;
+    if (x > 0 && y > 0)
+        cout << 1;
+    else if (y > 0)
+        cout << 2;
+    else if (x > 0)
+        cout << 4;
+    else
+        cout << 3;
+
+    return 0;
+}
